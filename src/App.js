b118166839530d813aaa4db7d65e5a28280857f6
Risa Takata <beat1422@icloud.com>
2020-02-02 21:34:59 -0800

    use map showing profiles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React from 'react';
 
 function App() {
-  const greeting = "hi lisa"
-  const dom = <h1>{greeting}</h1>
-  return dom;
-  }
+  const profiles = [
+    { name: "taro", age: 10 },
+    { name: "hanako", age: 5 },
+    {name:"lisa"}
+  ]
 
+  return (
+    <div>
+      {
+        profiles.map((profile,index) => {
+          return <User name={profile.name} age={profile.age} key={index} />
+        })
+      }
+    </div>
+  )
+}
+  
+const User = (props) => {
+  return (
+    <div>hi my name is{props.name},age is {props.age} </div>
+  )
+}
+
+User.defaultProps = {
+  age:1
+}
 export default App;
 
