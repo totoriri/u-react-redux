3ca6e64ef5d320cd4d0ad4e0ff13568870c777e8
Risa Takata <beat1422@icloud.com>
2020-02-04 14:12:21 -0800

    inplemented store in component

--- src/components/App.js
+++ src/components/App.js
@@ -1,47 +1,28 @@
 import React from 'react';
+import { connect } from "react-redux"
 
-function App() {
+import { increment, decrement } from "../actions"
+
   
-  return (
-    <div>
-     <Counter　/>
-    </div>
-  )
-}
-  
-class Counter extends React.Component{
-  constructor(props) {
-    super(props)
-    this.state = {
-      count:0
-    }
-  }
-
-  handlePlusButton = () => {
-    console.log("handlePlusButton")
-    this.setState({
-      count:this.state.count+1
-    })
-    console.log(this.state.count)
-  }
-
-  handleMinusBUtton = () => {
-    this.setState({
-      count:this.state.count-1
-    })
-  }
+class App extends React.Component{
   render() {
+    const props = this.props
     console.log("reander")
     return (
       <React.Fragment>
-        <div>counter:{this.state.count} </div>
-        <button onClick={this.handlePlusButton}>+1</button>
-        <button onClick={this.handleMinusBUtton}>-1</button>
+        <div>value:{props.value} </div>
+        <button onClick={props.increment}>+1</button>
+        <button onClick={props.decrement}>-1</button>
       </React.Fragment>
     )
    }
 }
 
+const mapStateToProps = state => ({ value: state.count.value })
+const mapDispatchToProps = dispatch => ({
+  increment: () => dispatch(increment()),
+  decrement:()=> dispatch(decrement())
+})
+export default connect(mapStateToProps,mapDispatchToProps)(App)
 
-export default App;
 
--- src/index.js
+++ src/index.js
@@ -11,7 +11,11 @@ import * as serviceWorker from './serviceWorker';
 
 const store =createStore(reducer)
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
