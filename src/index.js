01bf0bc5b83480c8c0f847676b55b994ecfcb72a
Risa Takata <beat1422@icloud.com>
2020-02-04 13:26:44 -0800

    created store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from "redux"
+import { Provider } from "react-redux"
+
 import './index.css';
-import App from './App';
+import reducer from "./reducers"
+
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+
+const store =createStore(reducer)
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
