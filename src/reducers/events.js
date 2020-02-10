a5520124b7a530e4398909328d04bf6b2d67d67b
Risa Takata <beat1422@icloud.com>
2020-02-09 22:05:42 -0800

    created events list page

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,12 +1,13 @@
+import _ from 'lodash'
 import { READ_EVENTS } from "../actions"
 
-const initialState = { value: 0 }
 
-export default (state = {}, action ) => {
+export default (events = {}, action ) => {
     switch (action.type) {
         case READ_EVENTS:
-            return state
+            console.log(action.response.data)
+        return _.mapKeys(action.response.data,'id')
         default:
-            return state;
+            return events;
     }
 }
