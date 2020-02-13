07f14f2372d65b95518ec78c38576d0e0c4fb4da
Risa Takata <beat1422@icloud.com>
2020-02-12 21:10:07 -0800

    inplemented redux form

--- src/components/event_new.jsx
+++ src/components/event_new.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { connect } from "react-redux"
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
+import {Field , reduxForm} from 'react-router-dom'
 
 // import { postEvents } from "../actions"
 
   
 class EventsNew extends React.Component{
+  renderField(field) {
+    const { input, label, type, meta: { touched, error } } = field
+    return(<div></div>)
+  }
   render() {
     return (
       <React.Fragment>
-      <div>foo</div>
+        <Field labe="Title" name="title" type="text" component={this.renderField} />
+        <Field labe="Title" name="title" type="text" component={this.renderField} />
        </React.Fragment>
     )
    }
